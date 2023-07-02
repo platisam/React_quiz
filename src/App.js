82e2c3c5faa07c711ad8dd428d39e56f01fbb5c9
platisam <platisam@gmail.com>
2023-07-02 23:03:33 +0200

    added DateCounter component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-function App() {
-  return <div className="App">React-quiz</div>;
-}
+import DateCounter from "./DateCounter";
 
-export default App;
+export default function App() {
+  return (
+    <div>
+      <DateCounter />
+    </div>
+  );
+}
