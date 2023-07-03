e426ba055b86c2a6d3b1eec5ac01797e3283fa44
platisam <platisam@gmail.com>
2023-07-03 16:56:02 +0200

    setting dispatch function with action object(type, payload)

--- src/DateCounter.js
+++ src/DateCounter.js
@@ -1,7 +1,11 @@
+import { type } from "@testing-library/user-event/dist/type";
 import { useReducer, useState } from "react";
 
 function reducer(state, action) {
   console.log(state, action);
+  if (action.type === "inc") return state + action.payload;
+  if (action.type === "dec") return state - action.payload;
+  if (action.type === "setCount") return action.payload;
 }
 
 function DateCounter() {
@@ -16,6 +20,7 @@ function DateCounter() {
   date.setDate(date.getDate() + count);
 
   const dec = function () {
+    dispatch({ type: "dec", payload: 1 });
     // setCount((count) => count - 1);
     //setCount((count) => count - step);
   };
@@ -23,10 +28,12 @@ function DateCounter() {
   const inc = function () {
     // setCount((count) => count + 1);
     //setCount((count) => count + step);
+    dispatch({ type: "inc", payload: 1 });
   };
 
   const defineCount = function (e) {
     //setCount(Number(e.target.value));
+    dispatch({ type: "setCount", payload: Number(e.target.value) });
   };
 
   const defineStep = function (e) {
