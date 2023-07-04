7d210a74130979151cbb7026403e1c5bc5891a79
platisam <platisam@gmail.com>
2023-07-04 14:31:15 +0200

    stored questions in reducer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import { useEffect } from "react";
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
 
+const initialState = {
+  questions: [],
+
+  // 'loading', 'error', 'ready', 'active', 'finished'
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    default:
+      throw new Error("Action unknown");
+  }
+}
+
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   useEffect(() => {
     fetch("http://localhost:8000/questions")
       .then((res) => res.json())
-      .then((data) => console.log(data))
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
       .catch((err) => console.error("Error"));
   }, []);
 
