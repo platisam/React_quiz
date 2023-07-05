87177b4c02b4d671addb1bfacb073ed015214c9a
platisam <platisam@gmail.com>
2023-07-06 01:51:02 +0200

    setting clean up function

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -2,9 +2,11 @@ import { useEffect } from "react";
 
 const Timer = ({ dispatch, secondsRemaining }) => {
   useEffect(() => {
-    setInterval(() => {
+    const id = setInterval(() => {
       dispatch({ type: "tick" });
     }, 1000);
+
+    return () => clearInterval(id);
   }, [dispatch]);
 
   return <div className="timer">{secondsRemaining}</div>;
