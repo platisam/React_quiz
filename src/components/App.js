ea08008bb37b46577d2627fa6f947a76150be41d
platisam <platisam@gmail.com>
2023-07-05 21:25:27 +0200

    passing points as a prop to Progress component

--- src/components/App.js
+++ src/components/App.js
@@ -53,7 +53,7 @@ function reducer(state, action) {
 }
 
 export default function App() {
-  const [{ questions, status, index, answer }, dispatch] = useReducer(
+  const [{ questions, status, index, answer, points }, dispatch] = useReducer(
     reducer,
     initialState
   );
@@ -80,7 +80,11 @@ export default function App() {
         )}
         {status === "active" && (
           <>
-            <Progress index={index} numQuestions={numQuestions} />
+            <Progress
+              index={index}
+              numQuestions={numQuestions}
+              points={points}
+            />
             <Question
               question={questions[index]}
               dispatch={dispatch}
--- src/components/Progress.js
+++ src/components/Progress.js
@@ -1,8 +1,11 @@
-const Progress = ({ index, numQuestions }) => {
+const Progress = ({ index, numQuestions, points }) => {
   return (
     <header className="progress">
       <p>
         Question <strong>{index + 1}</strong> / {numQuestions}
+      </p>
+      <p>
+        <strong>{points} / X</strong>
       </p>
     </header>
   );
