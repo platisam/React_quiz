c682669f66b33ff3c4bb89c44ab89d1a4ab080c9
platisam <platisam@gmail.com>
2023-07-05 16:43:55 +0200

    mapping through options array and displaying questions

--- src/Question.js
+++ src/Question.js
@@ -4,6 +4,15 @@ const Question = ({ question }) => {
   return (
     <div>
       <h4>{question.question}</h4>
+      <div className="options">
+        {question.options.map((option) => {
+          return (
+            <button className="btn btn-option" key={option}>
+              {option}
+            </button>
+          );
+        })}
+      </div>
     </div>
   );
 };
