ae53f89592b1aed924afccb1af65b1407060e356
platisam <platisam@gmail.com>
2023-07-05 18:08:43 +0200

    handling new answer and passing dispatch function and answer as props to Options component

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,9 +1,13 @@
-const Options = ({ question }) => {
+const Options = ({ question, dispatch, answer }) => {
   return (
     <div className="options">
-      {question.options.map((option) => {
+      {question.options.map((option, index) => {
         return (
-          <button className="btn btn-option" key={option}>
+          <button
+            className="btn btn-option"
+            key={option}
+            onClick={() => dispatch({ type: "newAnswer", payload: index })}
+          >
             {option}
           </button>
         );
