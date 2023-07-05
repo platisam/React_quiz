d29e7518f28144840fdd4e301a81263ff38f2b79
platisam <platisam@gmail.com>
2023-07-05 19:05:52 +0200

    added hasAnswered variable and disabled button based on nested ternary operators

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,13 +1,20 @@
 const Options = ({ question, dispatch, answer }) => {
+  const hasAnswered = answer !== null;
+
   return (
     <div className="options">
       {question.options.map((option, index) => {
         return (
           <button
             className={`btn btn-option ${index === answer ? "answer" : ""} ${
-              index === question.correctOption ? "correct" : "wrong"
+              hasAnswered
+                ? index === question.correctOption
+                  ? "correct"
+                  : "wrong"
+                : ""
             }`}
             key={option}
+            disabled={hasAnswered}
             onClick={() => dispatch({ type: "newAnswer", payload: index })}
           >
             {option}
