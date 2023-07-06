a32c5dfc500277fd148e1958e3e990f3492b31af
platisam <platisam@gmail.com>
2023-07-06 02:10:34 +0200

    timer formatting with conditional rendering

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,6 +1,9 @@
 import { useEffect } from "react";
 
 const Timer = ({ dispatch, secondsRemaining }) => {
+  const mins = Math.floor(secondsRemaining / 60);
+  const seconds = secondsRemaining % 60;
+
   useEffect(() => {
     const id = setInterval(() => {
       dispatch({ type: "tick" });
@@ -9,6 +12,12 @@ const Timer = ({ dispatch, secondsRemaining }) => {
     return () => clearInterval(id);
   }, [dispatch]);
 
-  return <div className="timer">{secondsRemaining}</div>;
+  return (
+    <div className="timer">
+      {mins < 10 && "0"}
+      {mins}:{seconds < 10 && "0"}
+      {seconds}
+    </div>
+  );
 };
 export default Timer;
